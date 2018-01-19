ae3009438ccd36fec3b6438b288063e3c6429bb3
Ashish Singh <melliferalabs@Melliferas-Mac-mini.local>
2018-01-19 19:49:14 +0530

    Lecture26

---
+++ desc.js
@@ -0,0 +1,15 @@
+/*
+    Angular JS      sudo npm install -g @angular/cli
+
+Melliferas-Mac-mini:untitled melliferalabs$ npm i @angular-devkit/core --save-dev
+
+for using ngModule....=  use imports: [
+  BrowserModule,
+  FormsModule
+],   in the    app/app.module.ts
+
+
+use ng g c servers to make new dir into the src folder name servers....
+
+
+*/
